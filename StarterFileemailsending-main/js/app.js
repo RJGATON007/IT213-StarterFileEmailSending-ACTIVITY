ee2493191ea47f9bb571bae2a455c9deeb3a7b8f
RJGATON007 <robertgaton2017@gmail.com>
2021-10-20 20:13:09 +0800

    validating field pt.2

--- StarterFileemailsending-main/js/app.js
+++ StarterFileemailsending-main/js/app.js
@@ -34,12 +34,39 @@ function validateField() {
     let errors;
 
     // Validate the length of the field
-    validateLength(this)
+    validateLength(this);
+
+    // validate the email
+    if(this.type === 'email') {
+        validateEmail(this);
+    }
+
+    // both will return the errors, then check if there are any errors
+    errors = document.querySelectorAll('.error');
+
+    // check if the inputs are not empty
+    if(email.value !== '' && subject.value !== '' && message.value !== '' ) {
+        if(errors.length === 0) {
+            // the button should be enabled
+            sendBtn.disabled = false;
+        }
+    }
 }
 //Validate the length of the fields
-
 function validateLength(field) {
     if(field.value.length > 0) {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
+}
+// validate email (checks for @ in the value)
+function validateEmail(field) {
+    let emailText = field.value;
+    // check if the emailText contains the @ sign
+    if(emailText.indexOf('@') !== -1) {
         field.style.borderBottomColor = 'green';
         field.classList.remove('error');
     } else {
